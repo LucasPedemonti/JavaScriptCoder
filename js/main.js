2997b5a2839c50a00dbc0c594536dff887cff6f0
Lucas Pedemonti <lucaspedemont91@gmail.com>
2022-09-27 00:49:22 -0300

    Arreglos

--- js/main.js
+++ js/main.js
@@ -12,7 +12,9 @@ function elegirServicio() {
     servicio1 = parseInt (prompt("El Servicio que elegiste no existe.\nPor favor volvé a intentarlo: \n1-Desarrollo Web  \n2-Arquitectura naval"));
   }
 }
+
 elegirServicio()
+console.log(servicio1);
 //Confirmar
 function confirmarServicio() {
   confirServ = prompt("Queres averiguar sobre opcion "+ servicio1+" \n1-Para Confirmar\n2-Para Volver a corregir");
@@ -26,7 +28,7 @@ function confirmarServicio() {
   }
 }
 confirmarServicio()
-
+if (servicio1=="1"){
 function opciones() {
   producto = prompt("1-Pagina Web \n2-Portfolio\n3-Tienda Ecommerse\n(N) Para cancelar \n(Y) Para enviar pedido");
   return producto;
@@ -54,6 +56,7 @@ function opPedido() {
       default:
         alert("Esta opcion no es valida")
     }
+    console.log(totalPresupuesto);
     opciones();
   }
  return totalPresupuesto;
@@ -62,10 +65,10 @@ function terminarPresupuesto() {
   if ((producto == "y" || producto == "Y") && totalPresupuesto != 0) {
     alert("Cotizacion de servicios enviado con éxito el total es $"+totalPresupuesto+ "\nA la brevedad me estare comunidando con usted");
   } else if ((producto == "y" || producto == "Y") && totalPresupuesto == 0) {
-    alert("No cargaste ningún producto");
+    alert("No cargaste ningún servicio");
   }
   if (producto == "n" || producto == "N") {
-    alert("Cancelaste el presupuest");
+    alert("Cancelaste el presupuesto");
     totalPresupuesto = 0;
   }
   return totalPresupuesto;
@@ -75,7 +78,9 @@ function enviarPresupuesto() {
   opPedido();
   terminarPresupuesto();
 }
+
 enviarPresupuesto();
+
 function salirApp() {
   let salir;
   do {
@@ -93,3 +98,71 @@ function salirApp() {
   return salir;
 }
 salirApp();
+}else{
+  function opciones2() {
+    producto = prompt("1-Render \n2-Plano 2D\n3-Calculo del eje propulsor \n(N) Para cancelar \n(Y) Para enviar pedido");
+    return producto;
+  }
+  let totalPresupuesto=0;
+  function opPedido2() {
+    while (
+      producto != "N" &&
+      producto != "n" &&
+      producto != "Y" &&
+      producto != "y" ) {
+      switch (producto) {
+        case "1":
+          alert("Seleccionaste Render valor $70000");
+          totalPresupuesto= totalPresupuesto+70000;
+          break;
+        case "2":
+          alert("Seleccionaste Plano 2D valor $60000");
+          totalPresupuesto= totalPresupuesto+60000;
+          break;
+        case "3":
+          alert("Seleccionaste Calculo del eje propulsor valor $30000")
+          totalPresupuesto= totalPresupuesto+30000;
+          break;
+        default:
+          alert("Esta opcion no es valida")
+      }
+      opciones2();
+    }
+   return totalPresupuesto;
+  }
+  function terminarPresupuesto2() {
+    if ((producto == "y" || producto == "Y") && totalPresupuesto != 0) {
+      alert("Cotizacion de servicios enviado con éxito el total es $"+totalPresupuesto+ "\nA la brevedad me estare comunidando con usted");
+    } else if ((producto == "y" || producto == "Y") && totalPresupuesto == 0) {
+      alert("No cargaste ningún Servicio");
+    }
+    if (producto == "n" || producto == "N") {
+      alert("Cancelaste el presupuesto");
+      totalPresupuesto = 0;
+    }
+    return totalPresupuesto;
+  }
+  function enviarPresupuesto2() {
+    opciones2();
+    opPedido2();
+    terminarPresupuesto2();
+  }
+  enviarPresupuesto2();
+  function salirApp2() {
+    let salir;
+    do {
+      salir = prompt("(1) Para nuevo presupuesto\n(2) Para Salir");
+      if (salir == "1") {
+        totalPresupuesto = 0;
+        enviarPresupuesto2();
+        salirApp2();
+      } else if (salir == "2") {
+        alert("Saludos \nLucas Pedemonti - Desarrollador web");
+      } else {
+        alert("Opción incorrecta!");
+      }
+    } while (salir != "2" && salir != "1");
+    return salir;
+  }
+  salirApp2();
+}
